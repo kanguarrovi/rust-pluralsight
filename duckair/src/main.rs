37cb55373ad67f574d4975851bc63ad499b44740
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-02-27 23:30:18 -0600

    String and String slices

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    // let location: [f32;2] = [0.0, 0.0];
-    // let location: [f32;2] = [41.4094069, -81.8546911];
-    let location = ("KCLE", 41.4094069, -81.8546911);
-    // Destructuring the array of the tuple.
-    let (name, latitude, longitude) = location
-    println!("Location name: {}, latitude: {}, longitude: {}", location.0, location.1, location.2);
+    let person_name_string = String::from("Jafet Soto");
+    let person_name_slice = &person_name_string;
+    let person_name_slice2 = person_name_string.as_str();
 }
