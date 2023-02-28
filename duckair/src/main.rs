382f27f826ed3c830b18dd662649b56fcb71de29
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-02-27 23:23:32 -0600

    Array and tuples

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // let location: [f32;2] = [0.0, 0.0];
+    // let location: [f32;2] = [41.4094069, -81.8546911];
+    let location = ("KCLE", 41.4094069, -81.8546911);
+    // Destructuring the array of the tuple.
+    let (name, latitude, longitude) = location
+    println!("Location name: {}, latitude: {}, longitude: {}", location.0, location.1, location.2);
 }
