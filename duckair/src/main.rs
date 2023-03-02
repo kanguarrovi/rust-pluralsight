326434e5e2f422fed80ce454bf8d32ecb1d1fd0d
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-01 21:19:14 -0600

    Variable Mutability

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,21 +1,7 @@
 //#![allow(unused_variables)]
 
 fn main() {
-    /*
-    let float_thirty_two: f32 = 17.2;
-    let unsigned_eight: u8 = 5;
-    let cast_unsigned_eight = unsigned_eight as f32;
-
-    let result = float_thirty_two/ cast_unsigned_eight;
-
-    println!("{}", result);
-
-     */
-
-    let number = u8 = 65;
-    let letter: char  = number as char;
-
-    println!("{}", letter);
+    let mut changeable_variable = 500;
 
 }
 
