3f310e7bc1ce4cbe6ecf9c3b012183468d488e04
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-01 21:03:31 -0600

    Variables

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,5 +1,11 @@
+//#![allow(unused_variables)]
+
 fn main() {
-    let person_name_string = String::from("Jafet Soto");
-    let person_name_slice = &person_name_string;
-    let person_name_slice2 = person_name_string.as_str();
+    //let my_variable_name: u32 = 0;
+    //let my_inferred_name = 0;
+    // let my_variable_name: 1_i8;
+
+    let _warning_variable = 0; // For unused variables.
+
 }
+
