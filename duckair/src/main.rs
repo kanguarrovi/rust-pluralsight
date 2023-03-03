e719fe36a121dc901442b5c63f13de1e8346c1df
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-02 22:09:45 -0600

    Math operators

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,7 +1,11 @@
 //#![allow(unused_variables)]
 
 fn main() {
-    let mut changeable_variable = 500;
-
+    let squared = i32::pow(8, 2);
+    let float_integer = f32::powi(6.5, 3);
+    let float_float = f32::powf(6.5, 3.14);
+    println!("integer: {}", squared);
+    println!("float to int: {}", float_integer);
+    println!("float to float: {}", float_float);
 }
 
