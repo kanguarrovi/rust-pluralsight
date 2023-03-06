120795f4597e895be79839dff9744328e895fd18
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-05 21:34:58 -0600

    src/main.rs

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,44 +1,66 @@
-//#![allow(unused_variables)]
+#![allow(unused_variables)]
 
-enum NavigationAids {
-    NDB(u16),
-    VOR(String, f32),
-    VORDME(String, f32),
-    FIX{name: String, latitude: f32, longitude: f32}
-}
+fn main() {
+    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
 
-fn print_nav_aid(navaid: &NavigationAids){
-    match navaid {
-        NavigationAids::NDB(khz) => {
-            println!("NDB frequency is {} kilohertz", khz);
-        }
-        NavigationAids::VOR(id, freq) => {
-            println!("VOR identifier is {} and it's frequency is {} kilohertz", id, freq)
-        }
-        NavigationAids::VORDME(id, freq) => {
-            println!("VORDME identifier is {} and it's frequency is {} kilohertz", id, freq);
-        }
-        NavigationAids::FIX {name, latitude, longitude} => {
-            println!("FIX {} is at latitude and {} longitude {}", name, latitude, longitude)
+    let route = [
+        ("KCLE", 41.4075, -81.851111),
+        ("LEYIR", 41.51030, -84.88080),
+        ("PIONS", 41.65390, -84.48190),
+        ("ZOSER", 41.72390, -84.78130),
+        ("MODEM", 41.72800, -84.89730),
+        ("BRYTO", 41.74170, -85.31320),
+        ("SEWTO", 41.74780, -85.51130),
+        ("GIJ", 41.76860, -86.31850),
+        ("NEPTS", 41.96750, -87.05300),
+        ("THORR", 42.12330, -87.60030),
+        ("OBK", 42.22140, -87.95160),
+        ("COTON", 42.31990, -89.31220),
+        ("DBQ", 42.40150, -89.31220),
+        ("VIGGR", 42.55520, -93.12410),
+        ("FOD", 12.61110, -94.29480),
+        ("ONL", 42.47050, -98.68690),
+        ("BFF", 41.89420, -103.48200),
+        ("OCS", 41.59020, -109.34200),
+        ("PUDVY", 41.54270, -109.34200),
+        ("WEGEM", 41.44560, -109.99000),
+        ("KSLC", 40.7861, -111.9822)
+    ];
+
+    let mut total_distance = 0.0;
+    // Generic data type
+    let mut previous_waypoint: Option<(&str, f64, f64)> = None;
+
+    for waypoint in route.iter(){
+        match previous_waypoint {
+            None => {
+                previous_waypoint = Option::from(waypoint.clone());
+                continue;
+            }
+            Some(previous_waypoint_value) => {
+                let previous_waypoint_radians = previous_waypoint_value.1.to_radians();
+                let waypoint_radians = waypoint.1.to_radians();
+
+                let delta_latitude = (previous_waypoint_value.1 - waypoint.1).to_radians();
+                let delta_longitude = (previous_waypoint_value.2 - waypoint.2).to_radians();
+
+                let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2)
+                    + previous_waypoint_radians.cos() * waypoint_radians.cos()
+                    * f64::powi((delta_longitude / 2.0).sin(), 2);
+
+                let central_angle = 2.0 * inner_central_angle.sqrt().asin();
+
+                let distance = EARTH_RADIUS_IN_KILOMETERS * central_angle;
+
+                total_distance += distance;
+                previous_waypoint = Option::from(waypoint.clone());
+
+                println!("The distance between {} and {} is {:.1} kilometers",
+                         previous_waypoint_value.0, waypoint.0, distance);
+            }
         }
     }
 }
 
-fn main() {
-    let ndb_uwl = NavigationAids::NDB(385);
-    let vor_dqn = NavigationAids::VOR(String::from("DQN"), 114.5);
-    let vor_dme_sgh = NavigationAids::VORDME(String::from("SGH"), 113.2);
-    let fix_tarry = NavigationAids::FIX {
-        name: String::from("TARRY"),
-        latitude: 40.0533,
-        longitude: -83.91367
-    };
-
-    print_nav_aid(&ndb_uwl);
-    print_nav_aid(&vor_dqn);
-    print_nav_aid(&vor_dme_sgh);
-    print_nav_aid(&fix_tarry);
-
-}
 
 
