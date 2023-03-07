ef9b2460ccd4c2681396c2dbde8b0c0a7d211c20
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-06 21:52:32 -0600

    Lifetimes

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,20 +1,16 @@
 #![allow(unused_variables)]
 
 fn main() {
-    let mut original = String::from("original value");
-    println!("\noriginal: \t\"{}\"\n", original);
-
-    {
-        let next = &mut original; //This is a pointer to the memory address.
-        *next = String::from("next value"); //Memory address
-        // Go to the memory address that stored 'next' and assign this value.
-
-        println!("inner scope next: \t\"{}\"", next);
-        println!("inner scope original: \t\"{}\"", original);
-    }
-
-    println!("Outer original value: \t\"{}\"\n", original);
+    let value_one = 24;
+    let value_two = 67;
+    let value = explicit_lifetime(&value_one, &value_two);
 }
 
-
+fn explicit_lifetime<'a>(p1: &'a i32, p2: &'a i32) -> &'a i32 {
+    if p1 > p2 {
+        p1
+    } else {
+        p2
+    }
+}
 
