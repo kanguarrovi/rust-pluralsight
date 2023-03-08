5fc190dc2d734bd3d48e5d896c8886ce4cb9292f
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-07 20:53:18 -0600

    Error handling

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,23 +1,9 @@
 
 fn main(){
-    let mut original = String::from("original value");
-    println!("\n outer scope original: \t\"{}\"\n", original);
-
-    {
-        print_original(&original);
-        change_original(&mut original);
-        // This is the &mut that needs to be used when calling our change_original function.
-        println!("inner scope original: \t\"{}\"\n", original)
-    }
+    panic_vector();
 }
 
-
-fn print_original(original: &String){
-    print!("fn print_original:\t\"{}\"\n", original);
-}
-
-fn change_original(original: &mut String){
-    let next = original;
-    *next = String::from("Next value");
-    print!("fn change_original: \t\"{}\"\n", next);
+fn panic_vector(){
+    let vector = vec![1,2,3,4,5];
+    println!("{}", vector[10]);
 }
