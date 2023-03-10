1c712216263585a920e9d327b445f800c3e7eb43
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-09 22:04:30 -0600

    Traits

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,30 +1,61 @@
 #![allow(unused_variables)]
 
-struct Waypoint { //Capitalize every first letter of each word.
-    name: String,
-    latitude: f64,
-    longitude: f64
+struct Boeing {
+    required_crew: u8,
+    range: u16
 }
 
-struct Segment {
-    start: Waypoint,
-    end: Waypoint
+struct Airbus {
+    required_crew: u8,
+    range: u16
+}
+
+trait Flight{
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool;
+}
+
+impl Flight for Boeing {
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
+        if (available_crew >= required_crew) && (range + 150 > distance) {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+impl Flight for Airbus {
+    fn is_legal(&self, required_crew: u8, available_crew: u8, range: u16, distance: u16) -> bool {
+        if (available_crew >= required_crew) && (range + 280 > distance) {
+            true
+        } else {
+            false
+        }
+    }
 }
 
 fn main(){
-    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
-
-    let mut kcle = Waypoint{
-        name: "KCLE".to_string(),
-        latitude: 41.4075,
-        longitude: -81.851111
+    let boeing = Boeing {
+        required_crew: 4,
+        range: 7370
     };
 
-    let ksle = Waypoint {
-        name: "KSLC".to_string(), // Overwrite
-      ..kcle // The name of the instance you want to reuse.
+    let airbus = Airbus {
+        required_crew: 7,
+        range: 5280
     };
 
-    //kcle.latitude
+    let boeing_is_legal = boeing.is_legal(
+        boeing.required_crew, 18,
+        boeing.range, 2385
+    );
 
+    let airbus_is_legal = airbus.is_legal(
+        airbus.required_crew, 3,
+        airbus.range, 2200
+    );
+
+    println!("Is the Boeing flight legal? {}\n Is the Airbus flight legal? {}",
+             boeing_is_legal,
+             airbus_is_legal);
 }
