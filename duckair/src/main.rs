1f967976e04a44d7cc63c3ebaa73998a7246acf3
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-11 14:27:40 -0600

    Sets

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,31 +1,19 @@
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main(){
 
-    let mut flights = HashMap::new();
+    let mut flights = HashSet::new();
 
-    flights.insert("DA918", ("11:12", "Orlando"));
-    flights.insert("DA428", ("12:05", "Salt LAke City"));
-    flights.insert("DA98", ("09:43", "London"));
-    flights.insert("DA115", ("06:20", "Boston"));
-    flights.insert("DA41", ("15:30", "Berlin"));
-    flights.insert("DA2815", ("17:11", "Nashville"));
+    flights.insert( ("DA918", "11:12", "Orlando"));
+    flights.insert(("DA428", "12:05", "Salt LAke City"));
+    flights.insert(("DA98", "09:43", "London"));
+    flights.insert(("DA115", "06:20", "Boston"));
+    flights.insert(("DA41", "15:30", "Berlin"));
+    flights.insert(("DA2815", "17:11", "Nashville"));
 
-    let flight_number = "DA2815";
-
-    if !flight.contains_key(flight_number){
-        flight.insert(flight_number, ("12:00", "puerto Rico"));
-    } else {
-        println!("Flight {} is already entered", flight_number);
-    }
-
-    let option = flights.get(flight_number);
-    let (time, destination) = option.unwrap();
-    println!("{} {}", time, destination);
-
-    for flight in flight.iter(){
-        println!("{:.?}", flight);
+    for flight in flights.iter() {
+        println!("{:?}", flight);
     }
 }
