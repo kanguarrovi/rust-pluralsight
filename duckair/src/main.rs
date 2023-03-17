f158602821929a3d47080c29d3d98942b1c758c3
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2023-03-16 18:40:56 -0600

    Thread comunication

--- duckair/src/main.rs
+++ duckair/src/main.rs
@@ -1,20 +1,41 @@
 #![allow(unused_variables)]
 
 use std::thread;
-use std::thread::Thread;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 
 fn main(){
-    let outer_scope = 412;
+    let (john_tx, john_rx) = mpsc::channel();
+    let (sarah_tx, sarah_rx) =  mpsc::channel();
 
-    let join_handle = thread::spawn( move || {
-        outer_scope * 2
+    let john_handle = thread::spawn(move || {
+        john_chat(sarah_tx, john_rx);
     });
 
-    let result = join_handle.join();
-    match result {
-        Ok(value) => {
-            println!("{}", value);
-        }
+    let sarah_handle = thread::spawn(move || {
+        sarah_chat(john_tx, sarah_rx);
+    });
+
+    match john_handle.join(){
+        Ok(_) => {}
         Err(_) => {}
     }
+
+    match sarah_handle.join(){
+        Ok(_) => {}
+        Err(_) => {}
+    }
+}
+
+fn sarah_chat(john_tx:Sender<&str>, sarah_rx:Receiver<&str>){
+    let result = sarah_rx.recv();
+    println!("{}", result.unwrap());
+    let _send_result = john_tx.send("Hello John.");
+
+}
+
+fn john_chat(sarah_tx:Sender<&str>, john_rx:Receiver<&str>){
+    let _send_result = sarah_tx.send("Hello Sarah.");
+    let result = john_rx.recv();
+    println!("{}", result.unwrap());
 }
